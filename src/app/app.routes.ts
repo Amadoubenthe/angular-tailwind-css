f17976e6ac7bff6f5156f457ca3a377a33bb6427
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2024-02-16 16:05:16 +0000

    Add reactive state feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reactive-state',
+    loadChildren: () =>
+      import('./features/reactive-state/reactive-state.routes').then(
+        (m) => m.reactiveStateRoutes
+      ),
+  },
+];
---
+++ src/app/features/reactive-state/reactive-state.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CandidatesComponent } from './components/candidates/candidates.component';
+
+export const reactiveStateRoutes: Routes = [
+  { path: '', title: 'LogIn Page', component: CandidatesComponent },
+];
